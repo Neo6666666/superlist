1c28fdc3b3a1a48a4817d9dc90b4e8cdad363055
Neo <NEO6666666@GMAIL.COM>
2019-06-22 23:32:13 +0700

    Functional test now check oportunity enter list element

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -21,17 +23,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # Title say to user that it To-Do list webpage
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
-
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
         # User enter new list element
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 
+            'Enter a to-do item'
+        )
 
         # User enter 'Buy some milk'
+        inputbox.send_keys('Buy some milk')
 
         # User press 'Enter'. Page refresh. Now page content line 
         # 'Buy some milk' as new element
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy some milk' for row in rows))
 
         # Text field still able to add anothe element
-
+        self.fail('Закончить тест!')
         # User enter 'Make scrambled eggs'
 
         # Page refresh. Both elements show.
