a6c6d4d161fdbe15f8b0ca587b61247e504dc4cb
Neo <NEO6666666@GMAIL.COM>
2019-06-24 22:50:54 +0700

    Register lists app. Refactor view and unit test.

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -2,21 +2,13 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
     """Home page test"""
-
-    def test_root_url_resolves_to_home_page_view(self):
-        """Test: root url resolves to home page view"""
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
     
     def test_homepage_returns_correct_html(self):
         "Test: Home page returns correct HTML"
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
