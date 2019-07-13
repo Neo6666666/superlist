e18d3d45643bac81e88550f187ced5c680108d26
Neo <NEO6666666@GMAIL.COM>
2019-07-13 15:26:18 +0700

    Pre-deploing state

--- superlist/functional_tests/tests.py
+++ superlist/functional_tests/tests.py
@@ -1,4 +1,5 @@
 import time
+import os
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -15,6 +16,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         """Установка"""
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         """Демонтаж"""
